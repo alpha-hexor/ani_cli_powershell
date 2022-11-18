451b90c9f92e4dcc5c83553521d7aece3c5ca563
unknown <itsalexwilliams69@gmail.com>
2022-11-17 23:42:14 -0800

    second commit

--- ani_cli.ps1
+++ ani_cli.ps1
@@ -41,7 +41,7 @@ function search_anime($name){
     $anime_results = $response.ParsedHtml.getElementsByClassName('name') | ForEach-Object { $_.getElementsByTagName('a')} |Select-Object -Expand nameProp
     
     Write-Host "[*]Results: "
-    for($i=0;$i -le ($anime_results.Length -1);$i++){
+    for($i=0;$i -lt $anime_results.Length;$i++){
         Write-Host $i":" -NoNewline
         Write-Host $anime_results[$i]
     }
@@ -54,66 +54,82 @@ function search_ep($name){
     $link=$main_link+"/category/$name"
     $response = Invoke-WebRequest -Uri $link
     $eps = get_group "ep_start = '0' ep_end = '(\d+)'>" $response.Content
-    $ep_to_watch = Read-Host -Prompt "[*]Available Episode(1-$eps)"
-    [string]$ep_to_watch 
+    $eps
 }
 
-function get_streaming_link($link){
+function get_quality($link){
     $j=0
     $response = Invoke-WebRequest -Uri $link
     $qualities = Select-String "RESOLUTION=[0-9]+x([0-9]+)" -InputObject [string]$response.rawcontent -AllMatches | ForEach-Object {$_.matches.Groups.value}
-    $l = Select-String ".*\.m3u8" -InputObject $response.rawcontent -AllMatches | ForEach-Object {$_.matches.Groups.value}
-    for($i=1;$i -le ($qualities.Length -1) ; $i+=2){
-        Write-Host $j":" -NoNewline
-        Write-Host $qualities[$i]"p"
-        $j++
-    }
-    $p = Read-Host -Prompt "[*]Enter your choice"
-    $f_link = $l[$p]
-    if($f_link -match "https://"){
-        $f_link = $f_link
+    if($qualities.Length -ne 0){
+        $l = Select-String ".*\.m3u8" -InputObject $response.rawcontent -AllMatches | ForEach-Object {$_.matches.Groups.value}
+        for($i=1;$i -lt $qualities.Length ; $i+=2){
+            Write-Host $j":" -NoNewline
+            Write-Host $qualities[$i]"p"
+            $j++
+        }
+        $p = Read-Host -Prompt "[*]Enter your choice"
+        $f_link = $l[$p]
+        if($f_link -match "https://"){
+            $f_link = $f_link
+        }
+        else{
+            $f_link = $link.Replace($link.Split("/")[-1],$f_link)
+        }
     }
     else{
-        $f_link = $link.Replace($link.Split("/")[-1],$f_link)
+        $f_link = $link
     }
 
     $f_link
 }
 
-$name=Read-Host -Prompt "[*]Enter anime name"
-$anime_to_watch =search_anime $name
-$ep_to_watch = search_ep $anime_to_watch
+function get_final_link($link){
+    $response = Invoke-WebRequest -Uri $link
+    $gogo_link = "https:" + (get_group 'data-video=\"(//gogohd.net/streaming.php?.*)\" >' $response.Content)
+    $gogo_id = get_group 'id=(.*)\&' $gogo_link
+    $response = Invoke-WebRequest -Uri $gogo_link
+    $crypto_data = get_group 'data-value=\"(.*)\"' $response.Content
+    $raw_crypto = aes_decrypt "37911490979715163134003223491201" $crypto_data
+    $full_crypto_shit = get_re '\&mip=.*' $raw_crypto
+    $id = aes_encrypt $gogo_id
 
-$link=$main_link+"/$anime_to_watch-episode-$ep_to_watch"
-$Response=Invoke-WebRequest -URI $link
+    #payload for ajax
+    $full_payload = "$id$full_crypto_shit&alias=$gogo_id"
 
-$gogo_link = "https:" + (get_group 'data-video=\"(//gogohd.net/streaming.php?.*)\" >' $Response.Content)
-$gogo_id = get_group 'id=(.*)\&' $gogo_link
+    $header=@{
+        'x-requested-with' = 'XMLHttpRequest'
+    }
+    $response= Invoke-WebRequest -Uri "https://gogohd.net/encrypt-ajax.php?id=$full_payload" -Headers $header
+    $x = $response.Content | ConvertFrom-Json
 
-
-#get request to the gogolink
-$Response=Invoke-WebRequest -Uri $gogo_link
-$crypto_data= get_group 'data-value=\"(.*)\"' $Response.Content
-
-$raw_crypto = (aes_decrypt "37911490979715163134003223491201" $crypto_data)
-$full_cryptodata = get_re '\&mip=.*' $raw_crypto
-
-
-$id= aes_encrypt $gogo_id
-
-#make ajax request
-$full_payload="$id$full_cryptodata&alias=$gogo_id"
-
-$header=@{
-    'x-requested-with' = 'XMLHttpRequest'
+    $link = (aes_decrypt "54674138327930866480207815084989" $x[0].data).Trim("\\\\")
+    $final_link = (get_group 'file\":\"([^\"]*)' $link).Replace('\/','/')
+    $streaming_link = get_quality $final_link
+    $streaming_link 
 }
 
-$response= Invoke-WebRequest -Uri "https://gogohd.net/encrypt-ajax.php?id=$full_payload" -Headers $header
+function stream_episode($name,$ep_to_watch,$last_ep){
+    $anime_url=$main_link+"/$name-episode-$ep_to_watch"
+    $s = get_final_link $anime_url
+    Start-Process mpv -ArgumentList "$s --force-media-title='$name-ep-$ep_to_watch'"
 
-$x = $response.Content | ConvertFrom-Json
+    #for next episode
+    if(([int]$ep_to_watch+1) -le [int]$last_ep){
+        $x = Read-Host -Prompt "[*]Do you want to start next ep(y/n)"
+        if($x -like "n"){
+            Exit
+        }
+        $next_ep = [int]$ep_to_watch +1
+        stream_episode $name $next_ep $last_ep
+    }
 
-$link = (aes_decrypt "54674138327930866480207815084989" $x[0].data).Trim("\\\\")
-$final_link = (get_group 'file\":\"([^\"]*)' $link).Replace('\/','/')
-#Write-Host $final_link
-$final_streaming = get_streaming_link $final_link
-Start-Process mpv -ArgumentList "$final_streaming --force-media-title='$anime_to_watch-ep-$ep_to_watch'"
+
+
+}
+
+$name=Read-Host -Prompt "[*]Enter anime name"
+$anime_to_watch =search_anime $name
+$last_ep = search_ep $anime_to_watch
+$p = read-host -Prompt "[*]Available Episode(1-$last_ep)"
+stream_episode $anime_to_watch $p $last_ep
